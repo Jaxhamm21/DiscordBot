79a312b44406abf9849d0760459653663f94cb9e
Jaxhamm21 <122050289+Jaxhamm21@users.noreply.github.com>
2023-01-18 15:18:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions} = require("discord.js");
+
+const prefix = '/';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on("ready", () => {
+    console.log("Bot is Online!");
+
+    client.user.setActivity("Tutorial Bot", {type: "WATCHING" });
+})
+
+client.on("messageCreate" , (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    //Message Array
+
+    const messageArray = message.content.split(" ");
+    const argument = messageArray.slice(1);
+    const cmd = messageArray[0];
+
+    //COMMANDS
+
+
+
+
+})
+
+if (command === 'ping') {
+    message.channel.send("pong")
+} 
+
+//Token Login
+client.login("MTA2MzUxMDI5NzQxMjkxMTEyNA.GdiLFy.8fE7ixT1aVQAdFvC1yxDPIfn9hAgRzwC-y9RCU")
